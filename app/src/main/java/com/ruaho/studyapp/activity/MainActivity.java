8b13561c9ddff8fa8916dcc8e4b9b91757bd3bbc
yulin <yulin@ruaho.com>
2017-08-08 16:54:07 +0800

    Initial commit

---
+++ app/src/main/java/com/ruaho/studyapp/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ruaho.studyapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ruaho.studyapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
