623dd1abf58eb4cd96532cd3b0d767f0c0488fdd
yulin <yulin@ruaho.com>
2017-08-09 09:49:36 +0800

    布局

---
+++ app/src/main/java/com/ruaho/studyapp/activity/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.ruaho.studyapp.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by ruaho on 2017/8/9.
+ */
+
+public class BaseActivity extends Activity {
+
+
+}
--- app/src/main/java/com/ruaho/studyapp/activity/MainActivity.java
+++ app/src/main/java/com/ruaho/studyapp/activity/MainActivity.java
@@ -1,21 +1,45 @@
 package com.ruaho.studyapp.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
 
 import com.ruaho.studyapp.R;
-import com.ruaho.studyapp.utils.ToastsUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv_text = (TextView) findViewById(R.id.tv_text);
-        ToastsUtils.register(this);
+
 
     }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btn1:
+                break;
+            case R.id.btn2:
+                break;
+            case R.id.btn3:
+                break;
+            case R.id.btn4:
+                break;
+            case R.id.btn5:
+                break;
+            case R.id.btn6:
+                break;
+            case R.id.btn7:
+                break;
+            case R.id.btn8:
+                break;
+            case R.id.btn9:
+                break;
+            case R.id.btn10:
+                break;
+
+        }
+    }
+
 }
