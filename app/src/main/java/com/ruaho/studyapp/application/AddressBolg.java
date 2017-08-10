41d26dc0c68b0cfcabfc3a3cbf20140dfc5f25b3
yulin <yulin@ruaho.com>
2017-08-10 15:27:58 +0800

    Android值得学习的blog

---
+++ app/src/main/java/com/ruaho/studyapp/application/AddressBolg.java
@@ -0,0 +1,39 @@
+package com.ruaho.studyapp.application;
+
+/**
+ * Created by ruaho on 2017/8/10.
+ */
+
+public class AddressBolg {
+    /*
+    1:给每一个Android程序员——最新最全的Android开源项目集合
+        http://www.php230.com/1493769866.html
+    2:2017年Android开源项目及库汇总
+        http://blog.csdn.net/jsonnan/article/details/62215287
+    3:开发中最常用的GitHub上 优秀的 Android 开源项目整理（精品）
+        http://blog.csdn.net/lsyz0021/article/details/51234361
+    4:Android常用的信息
+        >1:Android开发常用的一些第三方网站
+            http://www.iteye.com/topic/1145521
+
+        >2:eoeandroid社区
+            http://www.eoeandroid.com/thread-546248-1-3.html
+
+        >3:Android 开源项目分类汇总
+            https://github.com/Trinea/android-open-project
+
+        >4:泡在网上的日子
+            http://www.jcodecraeer.com/
+
+        >5:猪八戒网仿微信朋友圈视频录制功能
+            http://task.zbj.com/5463222/
+
+        >6:提高安卓开发人员能力的20个开源项目
+            http://www.jcodecraeer.com/a/anzhuokaifa/androidkaifa/2017/0214/7114.html
+
+
+
+
+*/
+
+}
