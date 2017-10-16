b1e5e4132c8fea26692a555adde633ca53b17fe8
yulin <yulin@ruaho.com>
2017-10-16 17:37:49 +0800

    众信需要完成的功能

---
+++ app/src/main/java/com/ruaho/studyapp/work/CochatWork.java
@@ -0,0 +1,14 @@
+package com.ruaho.studyapp.work;
+
+/**
+ * Created by ruaho on 2017/10/16.
+ */
+
+public class CochatWork {
+    /**
+     * 众信为要完成的任务
+     *      1:同事圈 谁可以看 (权限) 界面实现功能 照着这个做
+     *          链接:http://blog.csdn.net/itarchy/article/details/49641791
+     */
+
+}
