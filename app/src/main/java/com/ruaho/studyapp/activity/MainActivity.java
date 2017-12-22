dc1506a56c641df769f95577c482e83d82ce26c3
yulin <yulin@ruaho.com>
2017-12-22 18:17:43 +0800

    启动原理

--- app/src/main/java/com/ruaho/studyapp/activity/MainActivity.java
+++ app/src/main/java/com/ruaho/studyapp/activity/MainActivity.java
@@ -2,19 +2,23 @@ package com.ruaho.studyapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.ruaho.studyapp.R;
 import com.ruaho.studyapp.goodcode.SocketConnection.activity.CochatSocketActivity;
 
+import java.util.LinkedHashMap;
+
 public class MainActivity extends BaseActivity {
+
+
+    private static LinkedHashMap<String, String> _cachedGroups = new LinkedHashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this is git ceshi!
-
 
     }
 
@@ -27,6 +31,11 @@ public class MainActivity extends BaseActivity {
                 startActivity(new Intent(MainActivity.this, CochatSocketActivity.class));
                 break;
             case R.id.btn2:
+                _cachedGroups.put("key","value");
+                _cachedGroups.put("key","value_123");
+
+                Log.e("输出","数据");
+
                 break;
             case R.id.btn3:
                 break;
