59dc819c106db63aa17d47b846478c453b51841b
yulin <2441518624@qq.com>
2019-06-03 11:10:27 +0800

    修改 tencent bugly 报错

--- app/src/main/java/com/ruaho/studyapp/application/App.java
+++ app/src/main/java/com/ruaho/studyapp/application/App.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 import com.ruaho.studyapp.db.GreenDaoManager;
-import com.tencent.bugly.crashreport.CrashReport;
+//import com.tencent.bugly.crashreport.CrashReport;
 
 /**
  * Created by ruaho on 2017/8/9.
@@ -22,7 +22,7 @@ public class App extends Application {
         //GreenDao的初始化
         GreenDaoManager.getInstance();
 //        CrashReport.initCrashReport(getApplicationContext(), "注册时申请的APPID", false);
-        CrashReport.initCrashReport(getApplicationContext(), "18ebe0368f", true);//建议在测试阶段建议设置成true，发布时设置为false。
+//        CrashReport.initCrashReport(getApplicationContext(), "18ebe0368f", true);//建议在测试阶段建议设置成true，发布时设置为false。
 
     }
 
